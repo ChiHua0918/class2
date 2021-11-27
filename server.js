3b578124600a8573672afc1d517d9e2e6a1286b2
chen_chi_hua <s108213042@mail1.ncnu.edu.tw>
2021-11-27 23:34:58 +0800

    express --view=ejs 架構

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ var server = http.createServer(function (req, res) {   // 2 - 建立server
     res.end();
 });
  
-server.listen(5000); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 的 xxxx
+server.listen(3000); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 的 xxxx
  
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at port 3000 is running..')
