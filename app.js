08191778fc27114a7969a36cf6b9c36f1311e17f
chen_chi_hua <s108213042@mail1.ncnu.edu.tw>
2021-12-11 22:20:29 +0800

    new

--- app.js
+++ app.js
@@ -3,11 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+let db_config = {
+  database: 'todolist',
+  host: 'localhost',
+  user: 'root',
+  password: ''
+}
+const connection = mysql.createConnection(db_config);
+connection.connect(function (err) {
+  if (err) {
+    console.log('connecting error');
+    return;
+  }
+  console.log('connecting success');
+});
+
+// 把 db 的連線綁定到 req 裡面
+app.use(function(req, res, next) {
+  req.db = connection;//新增req 的 DB屬性 connection
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '軟工課輔' });
+// 真的建立連線
+// req.db.connect();
+// 監聽 router (連線)
+router.get('/', function (req, res, next) {
+  // query
+  req.db.query('SELECT * from user WHERE id = 1', function (err, rows, fields) {
+    // 如果有 error 就會終止程式 (throw err)
+    if (err) console.log(err);
+    // 物件.屬性
+    console.log(rows[0].id);
+    res.render('index', { title: '軟工課輔',username:rows[0].username});
+  });
 });
+// req.db.query('INSERT INTO `user`(`username`, `passwd`) VALUES (?,?)',function(err,rows,fields){
+//   let username = "rita";
+//   let passwd = "123";
+//   return username,passwd;
+// })
 
+// connection.end();
 module.exports = router;
